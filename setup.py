1de6fb0e39f39ad2b047116668ab44de0a5429e3
Jan Petykiewicz <anewusername@gmail.com>
2018-09-16 20:12:52 -0700

    Use readme as long_description

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@
 from setuptools import setup, find_packages
 import opencl_fdtd
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name='opencl_fdtd',
       version=opencl_fdtd.version,
       description='OpenCL FDTD solver',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Jan Petykiewicz',
       author_email='anewusername@gmail.com',
       url='https://mpxd.net/code/jan/opencl_fdtd',
